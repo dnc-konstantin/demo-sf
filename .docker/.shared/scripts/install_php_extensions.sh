8216bd2627db27db52a649afa6fdaea91028858b
K <dnc.konstantin@gmail.com>
2021-12-23 11:58:00 +0200

    Init docker setup - Nginx, PHP, MySQL

---
+++ .docker/.shared/scripts/install_php_extensions.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# add wget
+apt-get update -yqq && apt-get -f install -yyq wget
+
+# download helper script
+# @see https://github.com/mlocati/docker-php-extension-installer/
+wget -q -O /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions \
+    || (echo "Failed while downloading php extension installer!"; exit 1)
+
+# install extensions
+chmod uga+x /usr/local/bin/install-php-extensions && sync && install-php-extensions \
+    intl \
+    mysqli \
+    opcache \
+    pdo_mysql \
+    xdebug \
+    @composer-2.2.1 \
+;
---
+++ README.md
@@ -0,0 +1,19 @@
+# Nginx-PHP-MySQL Docker Starter Kit
+
+## Docker help
+```
+make help
+```
+
+## Getting started
+Clone repository:
+```
+git clone https://github.com/dnc-konstantin/demo-sf.git
+cd demo-sf
+make docker-init
+```
+
+Update settings in `.docker/.env` and run:
+```
+make docker-build-from-scratch
+```
